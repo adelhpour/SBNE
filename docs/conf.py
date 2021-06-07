95ee9491d9f442f1f8e915da4b474bb7372084de
Adel Heydarabadipour <36909449+adelhpour@users.noreply.github.com>
2021-06-07 17:57:47 +0430

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -10,9 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('/images'))
 
 
 # -- Project information -----------------------------------------------------
@@ -91,4 +91,3 @@ if read_the_docs_build:
     configureDoxyfile(input_dir, output_dir)
     subprocess.call('doxygen', shell=True)
     breathe_projects['SBNE'] = output_dir + '/xml'
-    html_extra_path = ['doxygen-output/html']
