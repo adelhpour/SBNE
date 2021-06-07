4068fedb9d8355c1177e841f386b92cd8f082fec
Adel Heydarabadipour <36909449+adelhpour@users.noreply.github.com>
2021-06-07 21:18:50 +0430

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -10,9 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
@@ -68,7 +68,7 @@ html_static_path = ['_static']
 
 
 # Use on readthedocs
-import subprocess, os
+import subprocess
 
 def configureDoxyfile(input_dir, output_dir):
     with open('Doxyfile.in', 'r') as file :
