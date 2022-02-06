3aa4233389a88f17a11880745e1148e477b04d46
adelhpour <>
2022-02-06 18:30:40 +0330

    docs settings are configured

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Adel Heydarabadipour'
 author = 'Adel Heydarabadipour'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '0.2.0'
 
 
 # -- General configuration ---------------------------------------------------
